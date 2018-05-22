f97ffb8919ce892884c52715889af97dcde5e581
maksym.savchenko <maksym.savchenko@betlab.com>
2018-05-22 18:15:09 +0300

    Remove cron

--- run.sh
+++ run.sh
@@ -89,8 +89,3 @@ if [ -n "${INIT_BACKUP}" ]; then
     echo "=> Create a backup on the startup"
     /backup.sh
 fi
-
-echo "${CRON_TIME} /backup.sh >> /mongo_backup.log 2>&1" > /crontab.conf
-crontab  /crontab.conf
-echo "=> Running cron job"
-exec cron -f
