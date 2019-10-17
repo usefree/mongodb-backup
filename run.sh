94b978f63f29bbd45f1b24c09a3773fe4ee75758
maksym.savchenko <maksym.savchenko@pm.bet>
2019-10-17 11:42:51 +0300

    add support backup all cluster

--- run.sh
+++ run.sh
@@ -19,20 +19,29 @@ aws configure set default.s3.signature_version s3v4
 
 [[ ( -n "${MONGODB_USER}" ) ]] && USER_STR=" --username=${MONGODB_USER}"
 [[ ( -n "${MONGODB_PASS}" ) ]] && PASS_STR=" --password=${MONGODB_PASS}"
-[[ ( -n "${MONGODB_DB}" ) ]] && DB_STR=" --db=${MONGODB_DB}"
-[[ ( -n "${MONGODB_DB_SUFFIX}" ) ]] && EXTRA_OPTS_RESTORE=" --nsFrom ${MONGODB_DB}.* --nsTo ${MONGODB_DB}${MONGODB_DB_SUFFIX}.*"
+if [[ ( -z "${MONGODB_DB}" ) ]]; then
+  COMMAND="printjson(db.adminCommand( { listDatabases: 1, nameOnly: true } ).databases)"
+  DB_LIST=`mongo --host=${MONGODB_HOST} --port=${MONGODB_PORT} ${USER_STR}${PASS_STR} --quiet --eval "${COMMAND}" | sed '/^[0-9]/ d' | jq -r '.[].name' | grep -i -v -E  "admin|config|local"`
+else
+  DB_LIST="${MONGODB_DB}"
+fi
 
 
-BACKUP_CMD="mongodump --gzip --archive=/backup/"'${BACKUP_NAME}'" --host=${MONGODB_HOST} --port=${MONGODB_PORT} ${USER_STR}${PASS_STR}${DB_STR} ${EXTRA_OPTS}"
-UPLOAD_CMD="aws --endpoint-url $AWS_ENDPOINT_URL  s3 cp  /backup/"'${BACKUP_PATH}'"  s3://$AWS_BUCKET/ "
-DOWNLOAD_CMD="aws --endpoint-url $AWS_ENDPOINT_URL  s3 s3://$AWS_BUCKET/mongodump-latest.gz $BACKUP_URL/mongodump-latest.gz"
-CREATE_BUCKET_CMD="aws --endpoint-url $AWS_ENDPOINT_URL s3 mb s3://$AWS_BUCKET"
+for DB in ${DB_LIST}; do
+    [[ ( -n "${DB}" ) ]] && DB_STR=" --db=${DB}"
+    [[ ( -n "${MONGODB_DB_SUFFIX}" ) ]] && EXTRA_OPTS_RESTORE=" --nsFrom ${DB}.* --nsTo ${DB}${MONGODB_DB_SUFFIX}.*"
 
-echo "=> Creating backup script"
-rm -f /backup.sh
-cat <<EOF >> /backup.sh
+    BACKUP_CMD="mongodump --gzip --archive=/backup/"'${BACKUP_NAME}'" --host=${MONGODB_HOST} --port=${MONGODB_PORT} ${USER_STR}${PASS_STR}${DB_STR} ${EXTRA_OPTS}"
+    UPLOAD_CMD="aws --endpoint-url $AWS_ENDPOINT_URL  s3 cp  /backup/"'${BACKUP_PATH}'"  s3://$AWS_BUCKET/ "
+    DOWNLOAD_CMD="aws --endpoint-url $AWS_ENDPOINT_URL  s3 cp s3://$AWS_BUCKET/mongodump-latest.gz ./mongodump-latest.gz"
+    CREATE_BUCKET_CMD="aws --endpoint-url $AWS_ENDPOINT_URL s3 mb s3://$AWS_BUCKET"
+
+    echo "=> Creating backup script for ${DB}"
+    rm -f /backup.sh
+    cat <<EOF >> /backup.sh
 #!/bin/bash
-BACKUP_NAME=\mongodump-$(date +\%Y.\%m.\%d).gz
+DB_NAME=`echo ${DB} | awk '{print tolower($0)}'`
+BACKUP_NAME=\${DB_NAME}-$(date +\%Y.\%m.\%d).gz
 BACKUP_PATH=\${BACKUP_NAME}
 
 function UPLOAD {
@@ -47,7 +56,7 @@ echo "=> Backup started"
 if ${BACKUP_CMD} ;then
     echo "   Backup succeeded"
     ${CREATE_BUCKET_CMD} && UPLOAD || UPLOAD
-    BACKUP_PATH=\mongodump-latest.gz
+    BACKUP_PATH=\${DB_NAME}-latest.gz
     mv /backup/\${BACKUP_NAME} /backup/\${BACKUP_PATH}
     UPLOAD
 else
@@ -56,31 +65,33 @@ else
 fi
 echo "=> Backup done"
 EOF
-chmod +x /backup.sh
+    chmod +x /backup.sh
 
-echo "=> Creating restore script"
-rm -f /restore.sh
-cat <<EOF >> /restore.sh
+    echo "=> Creating restore script"
+    rm -f /restore.sh
+    cat <<EOF >> /restore.sh
 #!/bin/bash
+DB_NAME=`echo ${DB} | awk '{print tolower($0)}'`
 if ${DOWNLOAD_CMD} ;then
     echo "  Download succeeded"
 else
     echo "  Download failed"
 fi
 echo "=> Restore database"
-if mongorestore  --gzip --host=${MONGODB_HOST} --port=${MONGODB_PORT} ${USER_STR}${PASS_STR} ${EXTRA_OPTS_RESTORE} --archive=\mongodump-latest.gz; then
+if mongorestore  --gzip --host=${MONGODB_HOST} --port=${MONGODB_PORT} ${USER_STR}${PASS_STR} ${EXTRA_OPTS_RESTORE} --archive=\${DB_NAME}-latest.gz; then
     echo "   Restore succeeded"
 else
     echo "   Restore failed"
 fi
 echo "=> Done"
 EOF
-chmod +x /restore.sh
+    chmod +x /restore.sh
 
-touch /mongo_backup.log
-tail -F /mongo_backup.log &
+    touch /mongo_backup.log
+    tail -F /mongo_backup.log &
 
-if [ -n "${INIT_BACKUP}" ]; then
-    echo "=> Create a backup on the startup"
-    /backup.sh
-fi
+    if [ -n "${INIT_BACKUP}" ]; then
+        echo "=> Create a backup on the startup"
+        /backup.sh
+    fi
+done
